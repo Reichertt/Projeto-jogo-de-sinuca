c08a177defe36f2507f97e2c4aa04fb5b7a525b2
Vitor Reichert Gonçalves <vitor.r.goncalves@hotmail.com>
2023-11-28 08:07:30 -0300

    V1

---
+++ script.js
@@ -0,0 +1,15 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Jogador", "Profissional"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+        // typing text animation script
+        var typed = new Typed(".typing-2", {
+            strings: ["Jogador", "Profissional"],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+    });
